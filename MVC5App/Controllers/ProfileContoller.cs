817864156ed93a8bc249e5ac03af58ec12731589
JamesCondon22 <c00207200@itcarlow.ie>
2018-05-01 09:53:24 +0100

    starter kit profile

---
+++ MVC5App/Controllers/ProfileContoller.cs
@@ -0,0 +1,17 @@
+﻿using MVC5App.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5App.Controllers
+{
+    public class ProfileController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
